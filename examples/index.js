f3f5f31650b48479f52389e5954311cd350657b7
mdtang <mdtang@umich.edu>
2017-07-06 15:14:46 -0700

    added carousel to tutorials modal and added funny placeholder image

--- examples/index.js
+++ examples/index.js
@@ -961,17 +961,16 @@ function generateAgriCultureButtons(inputData, codeName) {
 
 //Search layer data
 function myFunction() {
-            var input, filter, ul, li, a, i;
+            var input, ul, li, a, i;
             input = document.getElementById("myInput");
-            filter = input.value.toUpperCase();
             ul = document.getElementById("myUL");
             li = ul.getElementsByTagName("li");
             for (i = 0; i < li.length; i++) {
                 a = li[i].getElementsByTagName("a")[0];
                 if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    li[i].style.display = "";
+                    ul[i].style.display = "";
                 } else {
-                    li[i].style.display = "none";
+                    ul[i].style.display = "none";
 
                 }
             }
