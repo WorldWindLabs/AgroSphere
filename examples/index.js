dfa1d7eb3d86eb876203a44e6accf2ced1935e62
staceychen <stacetheace@yahoo.com>
2017-07-17 09:23:49 -0700

    put back gotolocation feature

--- examples/index.js
+++ examples/index.js
@@ -90,6 +90,30 @@ requirejs({paths:{
         // Create a layer manager for controlling layer visibility.
         var layerManager = new LayerManager(wwd);
 
+        // Set up to handle clicks and taps.
+        // The common gesture-handling function.
+        var handleClick = function (recognizer) {
+            // Obtain the event location.
+            var x = recognizer.clientX,
+                y = recognizer.clientY;
+
+            // Perform the pick. Must first convert from window coordinates to canvas coordinates, which are
+            // relative to the upper left corner of the canvas rather than the upper left corner of the page.
+            var pickList = wwd.pick(wwd.canvasCoordinates(x, y));
+
+            // If only one thing is picked and it is the terrain, tell the world window to go to the picked location.
+            if (pickList.objects.length == 1 && pickList.objects[0].isTerrain) {
+                var position = pickList.objects[0].position;
+                wwd.goTo(new WorldWind.Location(position.latitude, position.longitude));
+            }
+        };
+
+        // Listen for mouse clicks.
+        var clickRecognizer = new WorldWind.ClickRecognizer(wwd, handleClick);
+
+        // Listen for taps on mobile devices.
+        var tapRecognizer = new WorldWind.TapRecognizer(wwd, handleClick);
+
         // Web Map Service information from NASA's Near Earth Observations WMS
         //var serviceAddress = "http://sedac.ciesin.org/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities";
         // Named layer displaying Average Temperature data
