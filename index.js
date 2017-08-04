4118163fa7e9e6667762993100665fde0bc94bc4
staceychen <stacetheace@yahoo.com>
2017-08-04 16:00:31 -0700

    finished video tutorial

--- index.js
+++ index.js
@@ -2637,8 +2637,8 @@ requirejs({paths:{
                 $("#comp").hide();
                 $("#weather").hide();
                 $("#view").hide();
-                        $('#legend').hide();
-                        $('#legendtext').hide();
+								$('#legend').hide();
+								$('#legendtext').hide();
                 setTimeout(function() {checkTabs()}, 50);
             });
             $(".togglecountry").click(function () {
@@ -2650,8 +2650,8 @@ requirejs({paths:{
                 $("#comp").hide();
                 $("#weather").hide();
                 $("#view").hide();
-                        $('#legend').hide();
-                        $('#legendtext').hide();
+								$('#legend').hide();
+								$('#legendtext').hide();
                 setTimeout(function() {checkTabs()}, 50);
             });
             $(".togglestation").click(function () {
@@ -2663,8 +2663,8 @@ requirejs({paths:{
                 $("#comp").hide();
                 $("#weather").hide();
                 $("#view").hide();
-                        $('#legend').hide();
-                        $('#legendtext').hide();
+								$('#legend').hide();
+								$('#legendtext').hide();
                 setTimeout(function() {checkTabs()}, 50);
             });
             $(".togglegraphs").click(function () {
@@ -2676,10 +2676,8 @@ requirejs({paths:{
                 $("#comp").hide();
                 $("#weather").hide();
                 $("#view").hide();
-                        $('#legend').hide();
-                        $('#legendtext').hide();
-                //This is amazing but apparently you can't use the resize sensor
-                //unless the thing is present, go figure
+                $('#legend').hide();
+                $('#legendtext').hide();
                 var i = 0;
                 var j = 0;
                 var manyGraphs = $('#manyGraph > div');
@@ -2688,12 +2686,12 @@ requirejs({paths:{
                     if(manyGraphs[i].childNodes.length == 1) {
                         //Add the resize
                         console.log($(manyGraphs[i]).attr('id'));
-                        new ResizeSensor($('#' + $(manyGraphs[i]).attr('id')), function(){
-                            for(j = 0; j < manyGraphs.length; j++) {
-                                var gd = $(manyGraphs[j]).children()[0];
-                                Plotly.Plots.resize(gd);
-                                console.log('melo');
-                            }
+                        new ResizeSensor($('#' + $(manyGraphs[i]).attr('id')),
+								function(){
+									for(j = 0; j < manyGraphs.length; j++) {
+										var gd = $(manyGraphs[j]).children()[0];
+										Plotly.Plots.resize(gd);
+								}
                         });
                         var gd = $(manyGraphs[i]).children()[0];
                         Plotly.Plots.resize(gd);
@@ -2719,8 +2717,8 @@ requirejs({paths:{
                 $("#station").hide();
                 $("#weather").hide();
                 $("#view").hide();
-                        $('#legend').toggle();
-                        $('#legendtext').toggle();
+								$('#legend').toggle();
+								$('#legendtext').toggle();
                 setTimeout(function() {checkTabs()}, 50);
             });
             $(".toggleweather").click(function () {
@@ -2732,8 +2730,8 @@ requirejs({paths:{
                 $("#station").hide();
                 $("#comp").hide();
                 $("#view").hide();
-                        $('#legend').hide();
-                        $('#legendtext').hide();
+								$('#legend').hide();
+								$('#legendtext').hide();
                 setTimeout(function() {checkTabs()}, 50);
             });
             $(".toggleview").click(function () {
@@ -2745,8 +2743,8 @@ requirejs({paths:{
                 $("#station").hide();
                 $("#comp").hide();
                 $("#weather").hide();
-                        $('#legend').hide();
-                        $('#legendtext').hide();
+								$('#legend').hide();
+								$('#legendtext').hide();
                 setTimeout(function() {checkTabs()}, 50);
             });
             $(".togglewms").click(function () {
@@ -2758,47 +2756,41 @@ requirejs({paths:{
                 $("#comp").hide();
                 $("#weather").hide();
                 $("#view").hide();$('#legend').hide();
-                        $('#legendtext').hide();
+								$('#legendtext').hide();
                 setTimeout(function() {checkTabs()}, 50);
             });
             checkTabs();
           /* highlighting correct button for geocomparison and wms layers */
-          $('.geoCompButton').click(function() {
-              if ($('.geoCompButton').hasClass('active')) {
-                  var clickedButtonIsActive = $(this).hasClass('active');
-
-                  $('.geoCompButton.active').removeClass('active');
-
-                  if (!clickedButtonIsActive) {
-                      $(this).addClass('active');
-                  }
-          }
-              else {
+        $('.geoCompButton').click(function() {
+            if ($('.geoCompButton').hasClass('active')) {
+                var clickedButtonIsActive = $(this).hasClass('active');
+                $('.geoCompButton.active').removeClass('active');
+                if (!clickedButtonIsActive) {
+                    $(this).addClass('active');
+                }
+			} else {
                   $(this).addClass('active');
               }
-          });
-          $('.wmsButton').click(function() {
-              if ($('.wmsButton').hasClass('active')) {
-                  var clickedButtonIsActive = $(this).hasClass('active');
-
-                  $('.wmsButton.active').removeClass('active');
-
-                  if (!clickedButtonIsActive) {
-                      $(this).addClass('active');
-                  }
-          }
-              else {
-                  $(this).addClass('active');
-              }
-          });
-          $('input:checkbox').click(function() {
-              $(this).toggleClass('active');
-          });
-            $('#allButton').click(function() {
-              $('#toggleLegend').toggle();
-          });
-            $('#allButtonStation').click(function() {
-              $('#toggleLegendStation').toggle();
-          });
         });
+        $('.wmsButton').click(function() {
+            if ($('.wmsButton').hasClass('active')) {
+                var clickedButtonIsActive = $(this).hasClass('active');
+                $('.wmsButton.active').removeClass('active');
+				if (!clickedButtonIsActive) {
+                    $(this).addClass('active');
+                }
+            } else {
+                $(this).addClass('active');
+            }
+        });
+        $('input:checkbox').click(function() {
+            $(this).toggleClass('active');
+        });
+        $('#allButton').click(function() {
+            $('#toggleLegend').toggle();
+        });
+        $('#allButtonStation').click(function() {
+            $('#toggleLegendStation').toggle();
+        });
+    });
 });
