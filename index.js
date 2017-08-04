df9daa455a43aac52b31240865c37dfe05e713cb
Atreya Iyer <atreya.iyer@menloschool.org>
2017-08-04 11:26:01 -0700

    editing comments

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-/**
- * Created by mdtang on 6/6/17.
- */
-
 /*
  * Copyright (C) 2014 United States Government as represented by the Administrator of the
  * National Aeronautics and Space Administration. All Rights Reserved.
- */
-/**
- * @version $Id: index.js 3320 2015-07-15 20:53:05Z dcollins $
  */
 
 var APIKEY = '26fb68df7323284ea4430d8e4d3c60b1';
